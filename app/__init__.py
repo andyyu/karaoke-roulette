0be31522b1c0a10f60e78f517c479b970323253a
andyyu <andyhsyu@gmail.com>
2015-05-24 19:32:00 -0700

    dependencies

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
